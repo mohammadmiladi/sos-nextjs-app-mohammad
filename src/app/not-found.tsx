74b8db18bb6e3fba8a5c38c358f572dc5177409d
mohammad miladi <mohammadmiladi.ie@gmail.com>
2025-03-11 03:26:59 +0330

    add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <Typography color="primary" mt={2} textAlign={"center"} fontSize={"32px"}>
+            صفحه مورد نطر یافت نشد !
+        </Typography>
+    );
+};
+
+export default NotFound;
